614113b70db42dfd139445e5af6cff4f17129a01
KulSmuf <kepka.lukazs@gmail.com>
2021-04-27 23:09:18 +0200

    added system app files

---
+++ system/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_candidate = models.BooleanField(default=False)
+
+    username = None
+    email = models.EmailField('email address', unique=True)
+
+    name = models.CharField(max_length=50)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name',]
